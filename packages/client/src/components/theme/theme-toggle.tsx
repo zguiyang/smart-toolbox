e9a8121f5a8a245dc82393b814b660cc78c9106f
joyzhao <2770723534@qq.com>
2024-08-22 17:47:08 +0800

    upd: Added ui components and made others updates

--- packages/client/src/components/theme/theme-toggle.tsx
+++ packages/client/src/components/theme/theme-toggle.tsx
@@ -1,19 +1,17 @@
-"use client"
+import { Moon, Sun } from 'lucide-react';
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
-
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from '@/components/ui/dropdown-menu';
 
-export function ThemeModeToggle() {
-  const { setTheme } = useTheme()
+import { useTheme } from './theme-provider';
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
@@ -25,16 +23,10 @@ export function ThemeModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }

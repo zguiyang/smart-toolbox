6df19ce5dd089bb2d5d4c6e05eef225e05a22ffe
joyzhao <2770723534@qq.com>
2024-08-23 16:19:27 +0800

    chore: Added prettier support via eslint

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,4 +1,7 @@
 import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -7,7 +10,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, prettierConfig],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -16,6 +19,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      import: importPlugin,
+      prettier: prettierPlugin,
     },
     rules: {
       'prettier/prettier': ['error'],

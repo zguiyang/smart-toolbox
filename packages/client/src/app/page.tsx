f06a56d4ab54d50c839d95a4223c405b0a19953d
zguiyang <zhaoguiyang18@gmail.com>
2024-10-01 07:17:29 +0800

    refactor: react to next.js 80%

---
+++ packages/client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className={"app-layout-page"}>
+      <h1>hello, Next.js</h1>
+    </div>
+  );
+}
---
+++ packages/client_back/src/layouts/modules/layout-footer.tsx
@@ -0,0 +1,3 @@
+export function LayoutFooter() {
+  return <main className={'app-layout-footer'}>我是网站底部了</main>;
+}
